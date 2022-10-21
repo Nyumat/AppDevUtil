6935899ad83cb3bca748a1e89951215212fe9614
Thomas <nyumat18@gmail.com>
2022-10-20 19:46:03 -0700

    Added Dependencies from BirthdayCountdown

--- PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
+++ PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
@@ -1,1 +1,10 @@
-private const val BASE_URL = "https://api.thecatapi.com/v1/images/search"
+
+import retrofit
+import RetroFit
+
+private const val BASE_URL = "https://api.thecatapi.com/v1/images/search";
+
+val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
