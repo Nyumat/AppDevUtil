0d48ff54a7adf25414548cd2e53c3bd3d7e6a74d
Thomas <nyumat18@gmail.com>
2022-10-27 18:35:20 -0700

    Fixed Gradle Errors

--- PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
+++ PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
@@ -1,10 +1,35 @@
+package com.example.playgroundappdev.Shared.Utils
 
-import retrofit
-import RetroFit
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
-private const val BASE_URL = "https://api.thecatapi.com/v1/images/search";
+private const val BASE_URL = "https://api.thecatapi.com/v1/images/search"
 
-val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create())
-        .build()
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+private val retrofit =
+                Retrofit.Builder()
+                                .addConverterFactory(ScalarsConverterFactory.create())
+                                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                                .baseUrl(BASE_URL)
+                                .build()
+
+interface CatApiService {
+        @GET("/cats")
+        fun getCats(): Deferred<Response<ResponseBody>>
+}
+
+object CatApi {
+        val retrofitService: CatApiService by lazy { retrofit.create(CatApiService::class.java) }
+}
+
+
