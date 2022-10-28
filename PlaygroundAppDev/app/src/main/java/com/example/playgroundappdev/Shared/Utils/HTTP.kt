5da145929a091afcaabf5e39314cf0560a79a2ea
Jonathan Rockett <jonathansrockett@gmail.com>
2022-10-27 18:36:28 -0700

    added boilerplate functions

--- PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
+++ PlaygroundAppDev/app/src/main/java/com/example/playgroundappdev/Shared/Utils/HTTP.kt
@@ -2,9 +2,25 @@
 import retrofit
 import RetroFit
 
-private const val BASE_URL = "https://api.thecatapi.com/v1/images/search";
+//private const val BASE_URL = "https://api.thecatapi.com/v1/images/search";
+private const val BASE_URL = "https://api.thecatapi.com";
 
 val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
+
+interface HttpService {
+        @GET("/v1/images/search")
+        //fun getImagesAsync():
+
+        //@POST("")
+        //@HEADERS("")
+        //fun createImagesAsync()
+}
+
+object HttpApi {
+        val retrofitService: HttpService by lazy {
+                retrofit.create(HttpService::class.java)
+        }
+}
